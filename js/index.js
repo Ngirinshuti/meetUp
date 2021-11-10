26d0a4fc36894be3cda7427b1fe5cd491442bd83
ISHIMWE <ishimwevalentin3@gmail.com>
2021-11-10 11:36:52 +0200

    refreshed

--- js/index.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    let input_ = document.getElementsByTagName('input');
-    for (var i = 0; i < input_.length; i++) {
-        let placeholder = input_[i].placeholder;
-        input_[i].onfocus = function() { this.placeholder = ""; }
-        input_[i].onblur = function() { this.placeholder = placeholder; }
-    }
-})();
